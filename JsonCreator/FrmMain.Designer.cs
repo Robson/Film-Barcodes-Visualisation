d17f8af4961c0c257c6ea51570cac49596049215
Robson <relative@gmail.com>
2024-01-25 19:18:31 +0000

    Add the C# code for converting the frames to json

---
+++ JsonCreator/FrmMain.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace JsonCreator
+{
+	partial class FrmDisplay
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.SuspendLayout();
+            // 
+            // FrmDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 119);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmDisplay";
+            this.Text = "Done";
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
+
---
+++ JsonCreator/FrmMain.cs
@@ -0,0 +1,73 @@
+﻿namespace JsonCreator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Drawing;
+    using System.IO;
+    using System.Linq;
+    using System.Windows.Forms;
+
+    public partial class FrmDisplay : Form
+    {
+        private const string RootFolder = @"C:\Temp\Films\";
+        private const string OutputFilename = "data.js";
+
+        public FrmDisplay()
+        {
+            InitializeComponent();
+
+            File.WriteAllText(OutputFilename, "var data = [");
+
+            new FrameReader("Frankenstein", RootFolder + "frank");
+            new FrameReader("Midsommar", RootFolder + "mid");
+            new FrameReader("Oppenheimer", RootFolder + "oppen");
+
+            File.AppendAllText(OutputFilename, "\n];");
+        }
+
+        internal class FrameReader
+        {
+            internal FrameReader(string film, string folder)
+            {
+                Console.WriteLine("Processing: " + film);
+
+                var colours = new List<Color>();
+                var frames = Directory.GetFiles(folder, "*.png").OrderBy(a => GetIndexFromFilename(a));
+                Bitmap frame;
+                foreach (var filename in frames)
+                {
+                    frame = (Bitmap)Image.FromFile(filename);
+                    long amount = 0, red = 0, green = 0, blue = 0;
+                    for (int x = 0; x < frame.Width; x += 2)
+                    {
+                        for (int y = 0; y < frame.Height; y += 2)
+                        {
+                            amount++;
+                            var pixel = frame.GetPixel(x, y);
+                            red += pixel.R;
+                            green += pixel.G;
+                            blue += pixel.B;
+                        }
+                    }
+                    colours.Add(Color.FromArgb((int)(red / amount), (int)(green / amount), (int)(blue / amount)));
+                    frame.Dispose();
+                }
+
+                var line =
+                  "\n{\"Name\":\"" + film + "\",\"Colours\":[\"" +
+                  string.Join("\",\"", colours.Select(a => $"{a.R:X2}{a.G:X2}{a.B:X2}")) +
+                  "\"]},";
+
+                File.AppendAllText(OutputFilename, line);
+            }
+
+            private int GetIndexFromFilename(string filename)
+            {
+                filename = Path.GetFileNameWithoutExtension(filename);
+                filename = filename.Replace("frame_", "");
+                return int.Parse(filename);
+            }
+        }
+    }
+}
